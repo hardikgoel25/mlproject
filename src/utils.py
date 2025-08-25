dea2d33850a20d89726985c9f43ee303eebb6179
Hardik <hardikgoel025@gmail.com>
2025-08-25 15:06:38 +0530

    Data Transformation

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+import dill
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        logging.info("Exception occurred in save_object function")
+        raise CustomException(e, sys)
